b3dadb36cfbb1bc0e949e54f83a3c2576e7ec61f
Roderic Page <r.page@bio.gla.ac.uk>
2016-08-29 22:28:07 +0100

    Undo intercom.io

--- index.php
+++ index.php
@@ -1344,12 +1344,6 @@ function display_html_end()
   </div>
 </div>';
 
-   echo "<script>
-  window.intercomSettings = {
-    app_id: \"jxn4g8d6\"
-  };
-</script>
-<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/jxn4g8d6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>";
 	
 	echo "<script type=\"text/javascript\">
 

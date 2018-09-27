f5d7ce676dfc04b9391162c4478438b3c3a17ee9
Roderic Page <rdmpage@gmail.com>
2018-09-27 13:35:55 +0100

    Check for missing CouchDB documents

---
+++ upload_couchdb/README.md
@@ -0,0 +1,7 @@
+# Bulk upload to new CouchDB
+
+## Get list of document ids
+
+```
+curl http://direct.biostor.org:5984/biostor/_design/cleaning/_list/key/biostor > biostor.txt
+```
---
+++ upload_couchdb/missing.php
@@ -0,0 +1,97 @@
+<?php
+
+// Parse list of ids, check they exist, if not replicate
+
+require_once(dirname(dirname(__FILE__)) . '/couchsimple.php');
+
+
+$filename = 'biostor.txt';
+$filename = 'missing.txt';
+
+$skip = 0;
+$count = 1;
+
+$chunksize = 50;
+
+$missing = array();
+
+$done = false;
+
+$file_handle = fopen($filename, "r");
+while (!feof($file_handle) && !$done) 
+{
+
+	$id = trim(fgets($file_handle));
+	
+	//echo "$id\n";
+	
+	if ($couch->exists($id))
+	{
+		//echo "OK\n";
+	}
+	else
+	{
+		echo "missing $id\n";
+		$missing[] = $id;
+	}
+	
+	if (count($missing) > $chunksize)
+	{
+		$doc = new stdclass;
+		
+		
+		$doc->source = "biostor";
+		$doc->target = "http://admin:3h0kylo8ljfp@35.204.147.240:5984/biostor";
+		$doc->doc_ids = $missing;
+		
+		print_r($doc);
+		
+		$command = "curl http://direct.biostor.org:5984/_replicate -H 'Content-Type: application/json' -d '" . json_encode($doc) . "'";
+		
+		echo $command . "\n";
+		
+		system($command);
+		
+		echo "Sleep...\n";
+		sleep(2);		
+		
+		$missing=[];
+		
+	}
+	
+	// Give server a break every 100 items
+	if (($count++ % 100) == 0)
+	{
+		$rand = rand(100000, 300000);
+		echo "[$count]...sleeping for " . round(($rand / 1000000),2) . ' seconds' . "\n";
+		usleep($rand);
+	}	
+
+	
+	
+}
+
+// Left over?
+if (count($missing) > 0)
+{
+		$doc = new stdclass;
+		
+		
+		$doc->source = "biostor";
+		$doc->target = "http://admin:3h0kylo8ljfp@35.204.147.240:5984/biostor";
+		$doc->doc_ids = $missing;
+		
+		print_r($doc);
+		
+		$command = "curl http://direct.biostor.org:5984/_replicate -H 'Content-Type: application/json' -d '" . json_encode($doc) . "'";
+		
+		echo $command . "\n";
+		
+		system($command);
+
+
+}
+
+	
+?>	
+

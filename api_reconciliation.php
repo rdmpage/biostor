b792d29fc1615654165cc06570b017807f87c0e9
Roderic Page <rdmpage@gmail.com>
2018-09-28 11:54:55 +0100

    Use Elasticsearch for reconciliation API

--- api_reconciliation.php
+++ api_reconciliation.php
@@ -46,6 +46,8 @@ class BioStorService extends ReconciliationService
 		$this->defaultTypes[] = $type;
 	} 	
 		
+	/*
+	// Cloudant 
 	//----------------------------------------------------------------------------------------------
 	// Handle an individual query
 	function OneQuery($query_key, $text, $limit = 1, $properties = null)
@@ -109,6 +111,88 @@ class BioStorService extends ReconciliationService
 
 		
 	}
+	*/
+	
+	// Elastic 
+	//----------------------------------------------------------------------------------------------
+	// Handle an individual query
+	function OneQuery($query_key, $text, $limit = 1, $properties = null)
+	{
+		global $config;
+		
+		// clean text
+		$text = str_replace(':', '', $text);
+		$text = str_replace('"', '', $text);
+		
+		// BioStor search API
+		$url = 'http://biostor.org/api.php?q=' . urlencode($text);
+		
+		//file_put_contents('/tmp/q.txt', $url, FILE_APPEND);
+		
+		$json = get($url);
+		
+		//file_put_contents('/tmp/q.txt', $json, FILE_APPEND);
+
+		if ($json != '')
+		{
+			$obj = json_decode($json);
+			
+			if (isset($obj->hits))
+			{
+				for ($i = 0; $i < 3; $i++)
+				{
+					$row = $obj->hits->hits[$i];
+					// check 
+					
+					$v1 = finger_print($text);
+					
+					$hit_text = '';
+					
+					if (isset($obj->hits->hits[$i]->_source->search_result_data->creator))
+					{
+						$hit_text .= join(' ', $obj->hits->hits[$i]->_source->search_result_data->creator);
+						$hit_text .= ' ';
+					}
+					
+					$hit_text .= $obj->hits->hits[$i]->_source->search_result_data->name;
+					$hit_text .= $obj->hits->hits[$i]->_source->search_result_data->description;
+					
+					$hit_text = preg_replace('/Published\s+in\s+/', '', $hit_text);
+					$hit_text = preg_replace('/,\s+in\s+/', ' ', $hit_text);
+					$hit_text = preg_replace('/\s+volume/', '', $hit_text);
+					$hit_text = preg_replace('/,\s+pages\s+/', ' ', $hit_text);
+					
+					$v2 = finger_print($hit_text);
+					
+					$lcs = new LongestCommonSequence($v1, $v2);
+					$d = $lcs->score();
+					
+					// echo $d;
+					
+					$score = min($d / strlen($v1), $d / strlen($v2));
+					
+					if ($score > 0.80)
+					{
+						$hit = new stdclass;
+						$hit->id 	= str_replace('biostor-', '', $obj->hits->hits[$i]->_id);
+				
+						$hit->name 	= $obj->hits->hits[$i]->_source->search_result_data->name;
+				
+						$hit->score = $score;
+						$hit->match = true;
+						$this->StoreHit($query_key, $hit);
+					}				
+				
+				
+				}
+			}
+		}
+		
+
+		
+	}	
+	
+	
 	
 	
 }
